61536f8e6d816883ad96e10f54ee99483274f741
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-04-13 19:21:39 +0530

    added atlas

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 const express = require('express')
 const app = express()
 
+
+
+const uri = 'mongodb://Yashwant:Yashwant@cluster0-shard-00-00.xubvb.mongodb.net:27017,cluster0-shard-00-01.xubvb.mongodb.net:27017,cluster0-shard-00-02.xubvb.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-o4dwpd-shard-0&authSource=admin&retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true , useCreateIndex : true, useFindAndModify : false } )
+const Article = require('./models/article');
+
 const articleRouter = require('./routes/articles')
-
-const mongoose = require('mongoose')
-
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
-
-const Article = require('./models/article');
-mongoose.connect('mongodb+srv://Yashwant:Yashwant@170301@cluster0.xubvb.mongodb.net/blog?retryWrites=true&w=majority' , { useNewUrlParser: true,  useUnifiedTopology: true , useCreateIndex : true, useFindAndModify : false } )
-
-
 app.use(express.urlencoded({ extended : false }));
-
 app.use(express.json())
-
-
-
 app.set('view engine', 'ejs')
-
 
 
 
 
 app.get('/', async (req,res)=>{
 
-    const articles = await Article.find().sort({
-        createdAT : 'desc'
-    });
-    res.render('articles/index' , { articles : articles });
+     const articles = await Article.find().sort({
+       createdAT : 'desc'
+     });
+     res.render('articles/index' , { articles : articles });
+    
 })
 app.use('/articles', articleRouter)
+
+
 
 app.listen(5000);
