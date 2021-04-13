4fb99dc22ef3e2dc5348615d2f4ba44512928da6
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-04-13 20:46:15 +0530

    lower-case

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express()
 
 const uri = 'mongodb://Yashwant:Yashwant@cluster0-shard-00-00.xubvb.mongodb.net:27017,cluster0-shard-00-01.xubvb.mongodb.net:27017,cluster0-shard-00-02.xubvb.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-o4dwpd-shard-0&authSource=admin&retryWrites=true&w=majority';
 const mongoose = require('mongoose');
-mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true , useCreateIndex : true, useFindAndModify : false } )
-const Article = require('./models/article');
+try{
+    mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true , useCreateIndex : true, useFindAndModify : false } )
+}
+catch(e){
+
+  console.log(e);
+
+}
+    const Article = require('./models/article');
 
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
